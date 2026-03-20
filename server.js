70716aeafb17a2655a665811227c9ce742c537a1
Anwita R  Rao <anwitarajendra@gmail.com>
2026-03-20 18:23:17 +0530

    setting up express server structure and env config

---
+++ server.js
@@ -0,0 +1,31 @@
+﻿require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(express.json());
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error(' Database connection failed: ', err.message);
+  } else {
+    console.log(' Connected to AWS RDS MySQL successfully!');
+    connection.release();
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('  Server is running on port ' + PORT);
+});
